c672b3464ddb9a0f0c1517e66f608cecd96fd5ab
nocriteriaforaevaaaaa <aevaacharya9@gmail.com>
2023-12-18 20:30:56 +0545

    sixth

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const secondHand=document.querySelector('.second-hand');
-
+const minHand=document.querySelector('.minute-hand');
+const hourHand=document.querySelector('.hour-hand')
 
 
 function setDate() {
@@ -9,6 +10,12 @@ const seconds=now.getSeconds();
 const secDegrees=(((seconds/60)*360)+90);
 secondHand.style.transform=`rotate(${secDegrees}deg)`;
 
+const mins=now.getMinutes();
+const minDegrees=(((mins/60)*360))+90;
+minHand.style.transform=`rotate(${minDegrees}deg)`;
 
+const hrs=now.getHours();
+const hrDegrees=(((hrs/12))*360)+90;
+hourHand.style.transform=`rotate(${hrDegrees}deg)`;
 }
 setInterval(setDate,1000)
